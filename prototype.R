f3873aa8f89d26d94717c5e5346f8cd1b2f53f0b
Pablo Rodriguez <pablo.rodriguez.sanchez@gmail.com>
2018-01-22 17:55:23 +0100

    Proper encoding
    The previous version didn't parse correctly the special charachters

--- prototype.R
+++ prototype.R
@@ -2,7 +2,8 @@
 rm(list = ls())
 
 ## Download using read-only link
+# As seen here: https://stackoverflow.com/questions/42461806/getting-a-csv-read-into-r-though-a-shareable-google-drive-link
 key <- "1NEQkPGVakj7XGFUkWe8alNsyTulrJuPqgAROqaPoSi0"
 sheet <- "Hoja 1"
 link <- sprintf("https://docs.google.com/spreadsheets/d/%s/gviz/tq?tqx=out:csv&sheet={%s}", key, sheet)
-stuff <- read.csv(link)
+stuff <- read.csv(link, encoding='UTF-8')
