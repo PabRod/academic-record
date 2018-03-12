9a2110bd0b015fccc829895c9961b7f566abd8a7
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-03-12 17:57:01 +0100

    Added plot_map

--- auxs.R
+++ auxs.R
@@ -1,4 +1,7 @@
 parse_academic_production <- function(key) {
+  ## Download using read-only link
+  # As seen here: https://stackoverflow.com/questions/42461806/getting-a-csv-read-into-r-though-a-shareable-google-drive-link
+  
   sheet <- "Hoja 1"
   link <- sprintf("https://docs.google.com/spreadsheets/d/%s/gviz/tq?tqx=out:csv&sheet={%s}", key, sheet)
   raw <- read.csv(link, encoding='UTF-8', dec=',')
@@ -34,4 +37,13 @@ parse_academic_production <- function(key) {
   # Read money
   tidy <- mutate(tidy, 
                  Amount = as.numeric(Amount))
+}
+
+
+plot_map <- function(countries) {
+  library(maptools)
+  
+  data(wrld_simpl)
+  myCountries = wrld_simpl@data$NAME %in% countries
+  plot(wrld_simpl, xlim = c(0,10), ylim = c(35,60), bg='lightblue', col = c('lightgrey', 'olivedrab3')[myCountries+1])
 }
--- prototype.R
+++ prototype.R
@@ -1,9 +1,10 @@
 ## Clean environment
 rm(list = ls())
+library(tidyverse)
+
 source('auxs.R')
 
-## Download using read-only link
-# As seen here: https://stackoverflow.com/questions/42461806/getting-a-csv-read-into-r-though-a-shareable-google-drive-link
+## Get the data
 key <- "1DVLcKvMLVrdkn2kRO-X-_KADaqVnhM4yyWddP-UN-40"
 tidy <- parse_academic_production(key)
 
@@ -14,3 +15,14 @@ tidy <- parse_academic_production(key)
 
 ## Extract relevant information
 total_amount <- sum(tidy$Amount, na.rm = TRUE)
+
+sci_comm <- filter(tidy, Type == 'Article', 
+                 Description == 'Naukas' |
+                 Description == 'CCC' | 
+                 Description == 'Next Door' | 
+                 Description == 'Mapping ignorance')
+
+## Plot some results
+hist(sci_comm$Date, breaks=20)
+
+plot_map(tidy$Country)
