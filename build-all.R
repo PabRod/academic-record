21860d317644bb9c2cda11497e78954879ff2e55
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-04-19 17:51:39 +0200

    Added sci-comm.Rmd

--- build-all.R
+++ build-all.R
@@ -3,6 +3,7 @@ rm(list = ls())
 # Update header_with_date.md
 source('auxs.R')
 updateHeader('headers/header_base.md','headers/header_with_date.md')
+updateHeader('headers/base_sci-comm.md','headers/base_sci-comm_with_date.md')
 
 # Render the files
 library(rmarkdown)
@@ -13,3 +14,10 @@ render('academic-record.Rmd',
         output_dir = 'output',
         encoding = 'utf-8',
         clean = TRUE)
+
+render('sci-comm.Rmd', 
+       output_format = 'md_document', 
+       output_file = NULL,
+       output_dir = 'output',
+       encoding = 'utf-8',
+       clean = TRUE)
---
+++ headers/base_sci-comm.md
@@ -0,0 +1,8 @@
+Title: Scientific communication
+Date: 4/12/2018, 11:15:11 AM
+Category: About-me
+Tags: info
+Slug: sci-comm
+Lang: English
+Authors: Pablo Rodríguez-Sánchez
+Summary: Activities related with science communication
