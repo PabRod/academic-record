86e6edbfd718d4da57dca6f66b9c200c039cb0dd
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-04-20 09:17:02 +0200

    First integrated version

--- build-all.R
+++ build-all.R
@@ -15,9 +15,23 @@ render('academic-record.Rmd',
         encoding = 'utf-8',
         clean = TRUE)
 
-render('sci-comm.Rmd', 
+render('sci-comm-en.Rmd', 
        output_format = 'md_document', 
        output_file = NULL,
        output_dir = 'output',
        encoding = 'utf-8',
        clean = TRUE)
+
+# Copy the files in the appropriate locations
+file.copy('output/academic-record.md', 
+          '../../content/pages/academic-record.md', 
+          overwrite = TRUE)
+
+#input_images <- 'output/academic-record_files/figure-markdown_strict'
+#new_folder <- '../../content/images'
+#list_of_files <- list.files(input_images) 
+#file.copy(file.path(input_images, list_of_files), new_folder, overwrite = TRUE)
+
+file.copy('output/sci-comm-en.md', 
+          '../../content/pages/sci-comm-en.md', 
+          overwrite = TRUE)
