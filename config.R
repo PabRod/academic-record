7d2df1cfff03153472e22327f89c688afc1f4257
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-04-27 20:09:37 +0200

    New content: education-en

--- config.R
+++ config.R
@@ -1,13 +1,16 @@
 # Input RMardown files
 inputs <- c('sci-comm-en.Rmd',
-           'sci-comm-es.Rmd')
+           'sci-comm-es.Rmd',
+           'education-en.Rmd')
 
 # Corresponding headers (w/o "Modified" metadata)
 headers <- c('headers/base_sci-comm_en.md',
-             'headers/base_sci-comm_es.md')
+             'headers/base_sci-comm_es.md',
+             'headers/base_education_en.md')
 
 # Render only if active
 active <- c(TRUE,
+            TRUE,
             TRUE)
 
 # Output directory
@@ -15,6 +18,7 @@ output_dir <- '../../content/pages'
 
 # Output filenames (NULL for default: name.Rmd -> name.md)
 output_filenames <- c(NULL,
+                     NULL,
                      NULL)
 
 # Temporary header location
---
+++ headers/base_education_en.md
@@ -0,0 +1,9 @@
+Title: Education
+Date: 4/12/2018, 11:15:11 AM
+Category: About-me
+Tags: info
+Slug: education
+Lang: en
+Authors: Pablo Rodríguez-Sánchez
+Summary: Education
+Modified: 04/20/2018, 16:21:12
