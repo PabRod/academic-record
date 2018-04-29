db48e1cf0826e57f5586e6b4c57a3587992433fd
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-04-29 19:54:47 +0200

    Added some documentation to build-all.R

--- build-all.R
+++ build-all.R
@@ -1,16 +1,17 @@
 rm(list = ls())
 library(rmarkdown)
 
-# Update header_with_date.md
+# Load auxiliary files
 source('config.R')
 source('auxs.R')
 
+# Generate each of the n documents
 n <- length(config$inputs)
-
 for (i in 1:n) {
-  if (config$active[i]) {
-    updateHeader(config$headers[i], config$hdr_temp[i])
+  if (config$active[i]) { # Only render if active
+    updateHeader(config$headers[i], config$hdr_temp[i]) # Generate temporary header with current date and time
     
+    # Render the documents
     render(inputs[i], 
            output_format = config$output_formats[i], 
            output_file = config$output_filenames[i],
@@ -18,6 +19,6 @@ for (i in 1:n) {
            encoding = 'utf-8',
            clean = TRUE)
     
-    file.remove(config$hdr_temp[i])
+    file.remove(config$hdr_temp[i]) # Remove temporary file used for including current date and time in the header
   }
 }
